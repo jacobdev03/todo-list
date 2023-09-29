4202742ebe3d329e2e4d579c446f2e2d184b4b35
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-09-29 22:13:27 +0200

    add todo completion functionality

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -1,15 +1,38 @@
-export default function (todos) {
+import todoForm from './todoForm';
+
+export default function displayTodos(todos, project) {
   const todoContainer = document.querySelector('.todo-container');
-  todos.forEach((todo) => {
-    const newTodoDiv = createTodoDiv(todo);
+  todoContainer.innerHTML = '';
+  todos.forEach((todo, index) => {
+    const newTodoDiv = createTodoDiv(todo, index);
     todoContainer.appendChild(newTodoDiv);
   });
+  todoContainer.appendChild(todoForm(project));
 }
 
-function createTodoDiv(todo) {
+function createTodoDiv(todo, index) {
   const todoDiv = document.createElement('div');
-  todoDiv.innerHTML = `
-    <h2>${todo.title}</h2>
-  `;
+  const circle = document.createElement('i');
+  const todoText = document.createElement('p');
+  todoText.textContent = todo.title;
+  todoDiv.classList.add('todo');
+  circle.classList.add('fa-regular');
+
+  if (todo.isCompleted) {
+    circle.classList.toggle('fa-circle-check');
+    todoText.classList.toggle('cross-line');
+  } else {
+    circle.classList.toggle('fa-circle');
+  }
+  todoDiv.addEventListener('click', () => {
+    circle.classList.toggle('fa-circle');
+    circle.classList.toggle('fa-circle-check');
+    todoText.classList.toggle('cross-line');
+    todo.toggleCompleted();
+  });
+
+  todoDiv.appendChild(circle);
+  todoDiv.appendChild(todoText);
+
   return todoDiv;
 }
