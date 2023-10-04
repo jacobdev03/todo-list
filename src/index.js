1c704c86c5453aea6c8720461194691bdd910619
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-10-04 19:11:49 +0200

    create sample todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+import { createTodo } from './createTodo';
+import { createProject } from './createProject';
+import displayTodos from './displayTodos';
+import displayNav from './navigation';
+
+const projects = [];
+
+const allProject = createProject('All');
+const schoolProject = createProject('School');
+
+const sampleTodo1 = createTodo('Do homework', 'test', '2023-09-09', 'urgent', false, schoolProject);
+const sampleTodo2 = createTodo('Do dishes', 'test', '2023-09-09', 'urgent', false, allProject);
+
+schoolProject.addTodo(sampleTodo1);
+allProject.addTodo(sampleTodo2);
+
+projects.push(allProject);
+projects.push(schoolProject);
+
+displayTodos(allProject.listTodos());
+displayNav(projects);
+
+let jsonProjects = JSON.stringify(projects);
+localStorage.setItem('projects', jsonProjects);
+
+export { projects };
